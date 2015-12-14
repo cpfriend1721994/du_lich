6d143df87db86a8714c02943257b72ec471f129c
cpfriend1721994 <hedspi1721994@gmail.com>
2015-12-14 21:58:08 +0700

    User:admin_Pass:123456

---
+++ _dulic.php
@@ -0,0 +1,845 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Trang du lich</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet"  href="_css/bootstrap.min.css">
+<link rel="stylesheet"  href="_css/giaodien.css">
+<link rel="stylesheet"  href="_css/bootstrap-theme.min.css">
+</head>
+
+<body background="_anh/thu2.jpg">
+<script type="text/javascript" src="_js/jquery-1.11.3.min.js"> </script>
+	<script type="text/javascript" src="_js/bootstrap.min.js"> </script>
+	
+	<div id="main">
+		<div id="head">
+		
+			<div class="image">
+	   <img height="70%" width="100%" src="_anh/anh.jpg"  alt="Ảnh test" />
+	   </div>
+		
+			<div id="menu" style="background-color:	#CCCCCC" >
+				 <nav class="navbar " >
+                    <div class="navbar-header" >
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menuthu">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a href="" class="navbar-brand">Trang chủ</a>
+                    </div>
+ 
+                    <div class="navbar-collapse collapse" id="menuthu">
+					
+                        <ul class="nav navbar-nav" >
+                            <li class="dropdown">
+                                <a data-toggle="dropdown" href="">Tour trong nước<span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Hải Phòng</a></li>
+									<div class="divider"></div>
+                                    <li><a href="#">Đà nẵng </a></li>
+                                    <div class="divider"></div>
+                                    <li><a href="#">Hội an</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+ 
+ <ul class="nav navbar-nav">
+                            <li class="dropdown">
+                                <a data-toggle="dropdown" href="">Tour nước ngoài<span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Mỹ</a></li>
+									<div class="divider"></div>
+                                    <li><a href="#">Nhật Bản</a></li>
+                                    <div class="divider"></div>
+                                    <li><a href="#">Trung Quốc</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+						
+						<ul class="nav navbar-nav">
+                            <li class="dropdown">
+                                <a data-toggle="dropdown" href="">Khách sạn<span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Bình Dân</a></li>
+									 <div class="divider"></div>
+                                    <li><a href="#">3 sao</a></li>
+                                    <div class="divider"></div>
+                                    <li><a href="#">4 sao</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+						
+						<ul class="nav navbar-nav">
+                            <li class="dropdown">
+                                <a data-toggle="dropdown" href="">Vé máy bay<span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Viet nam e</a></li>
+									 <div class="divider"></div>
+                                    <li><a href="#">Viet jestar</a></li>
+                                    <div class="divider"></div>
+                                    <li><a href="#">fdfsg</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+						
+                        <ul class="nav navbar-nav">
+                            <li class="dropdown">
+                                <a data-toggle="dropdown" href="">Thuê xe<span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">4 chỗ</a></li>
+									 <div class="divider"></div>
+                                    <li><a href="#">8 chỗ</a></li>
+                                    <div class="divider"></div>
+                                    <li><a href="#">16 chỗ</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+						
+						 <div class="navbar-right">
+                                <a href="" class="btn btn-info navbar-btn">Đăng ký</a>
+                                <a href="" class="btn btn-info navbar-btn">Đăng nhập</a>
+                            </div>
+							
+			
+			
+		</div>
+		</div>
+		
+
+		    <div id="content">
+      
+	   <div class="content1">
+	   			
+				<div class="panel panel-primary">
+                
+				<div class="panel-heading">
+                    <h3 class="panel-title">Du lịch nước ngoài</h3>
+                </div>
+                
+				<div class="panel-body">
+	<div class= "form1">
+				<h4> <a href="">Du lịch Hàn Quốc: 4 ngày giá tốt (T11/2015)</a></h4>
+	</div>
+		<div class="thu">
+		
+			<div class="thu1">
+			<img width="100%" height="60%" src="_anh/anhdl1.jpg"  alt="Ảnh test" />
+		</div>
+		<div class="thu2">
+			<table>
+				<tbody>
+				<tr>
+					<td>
+						<strong> Giá từ </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	 1.400.000d
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Giời gian </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	4 Ngày 4 Đêm
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Khời hành </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	24/10;5,13,20/11
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Vận Chuyển </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	Máy bay Vietnam Airlines
+					</td>
+					</tr>
+						<tr>
+					<td>
+						<strong> Khách Sạn </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					  <img src="_anh/sao.gif" />
+					</td>
+					</tr>
+				</tbody>
+			</table>
+			</div>
+	
+				</div>
+				<div class= "clear"></div>
+		<div class="noidung">
+					<ul>
+					
+						<li >
+							<strong><a href=""> Du lich Singapo: 4 ngày giá tốt</a></strong> <span> ~8.500.000</span>
+						</li>
+						<li id>
+						 	<strong><a href=""> Du lich Singapo: 4 ngày giá tốt</a></strong> <span> ~8.500.000</span>
+						</li>
+						<li id>
+							<strong><a href=""> Du lich Singapo: 4 ngày giá tốt</a></strong> <span> ~8.500.000</span>
+						</li>
+						<li id>
+							<strong><a href=""> Du lich Singapo: 4 ngày giá tốt</a></strong> <span> ~8.500.000</span>
+						</li>
+					</ul>
+			</div>
+				</div>
+				
+      </div>
+	  </div>
+		
+		
+		   <div class="content1">
+	   			
+				<div class="panel panel-primary">
+                
+				<div class="panel-heading">
+                    <h3 class="panel-title">Du lịch nước ngoài</h3>
+                </div>
+                
+				<div class="panel-body">
+	<div class= "form1">
+				<h4> <a href="">Du lịch Hàn Quốc: 4 ngày giá tốt (T11/2015)</a></h4>
+	</div>
+		<div class="thu">
+		
+			<div class="thu1">
+			<img width="100%" height="60%" src="_anh/anhdl1.jpg"  alt="Ảnh test" />
+		</div>
+		<div class="thu2">
+			<table>
+				<tbody>
+				<tr>
+					<td>
+						<strong> Giá từ </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	 1.400.000d
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Giời gian </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	4 Ngày 4 Đêm
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Khời hành </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	24/10;5,13,20/11
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Vận Chuyển </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	Máy bay Vietnam Airlines
+					</td>
+					</tr>
+						<tr>
+					<td>
+						<strong> Khách Sạn </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					  <img src="_anh/sao.gif" />
+					</td>
+					</tr>
+				</tbody>
+			</table>
+			</div>
+	
+				</div>
+				<div class= "clear"></div>
+		<div class="noidung">
+					<ul>
+					
+						<li >
+							<strong><a href=""> Du lich Singapo: 4 ngày giá tốt</a></strong> <span> ~8.500.000</span>
+						</li>
+						<li id>
+						 	<strong><a href=""> Du lich Singapo: 4 ngày giá tốt</a></strong> <span> ~8.500.000</span>
+						</li>
+						<li id>
+							<strong><a href=""> Du lich Singapo: 4 ngày giá tốt</a></strong> <span> ~8.500.000</span>
+						</li>
+						<li id>
+							<strong><a href=""> Du lich Singapo: 4 ngày giá tốt</a></strong> <span> ~8.500.000</span>
+						</li>
+					</ul>
+			</div>
+				</div>
+				
+      </div>
+	  </div>
+	  
+	  
+	 
+	  
+	  
+	     <div class="content1">
+	   			
+				<div class="panel panel-primary">
+                
+				<div class="panel-heading">
+                    <h3 class="panel-title">Du lịch nước ngoài</h3>
+                </div>
+                
+				<div class="panel-body">
+	<div class= "form1">
+				<h4> <a href="">Du lịch Hàn Quốc: 4 ngày giá tốt (T11/2015)</a></h4>
+	</div>
+		<div class="thu">
+		
+			<div class="thu1">
+			<img width="100%" height="60%" src="_anh/anhdl1.jpg"  alt="Ảnh test" />
+		</div>
+		<div class="thu2">
+			<table>
+				<tbody>
+				<tr>
+					<td>
+						<strong> Giá từ </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	 1.400.000d
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Giời gian </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	4 Ngày 4 Đêm
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Khời hành </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	24/10;5,13,20/11
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Vận Chuyển </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	Máy bay Vietnam Airlines
+					</td>
+					</tr>
+						<tr>
+					<td>
+						<strong> Khách Sạn </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					  <img src="anh/sao.gif" />
+					</td>
+					</tr>
+				</tbody>
+			</table>
+			</div>
+	
+				</div>
+				<div class= "clear"></div>
+		<div class="noidung">
+					<ul>
+					
+						<li >
+							<strong><a href=""> Du lich Singapo: 4 ngày giá tốt</a></strong> <span> ~8.500.000</span>
+						</li>
+						<li id>
+						 	<strong><a href=""> Du lich Singapo: 4 ngày giá tốt</a></strong> <span> ~8.500.000</span>
+						</li>
+						<li id>
+							<strong><a href=""> Du lich Singapo: 4 ngày giá tốt</a></strong> <span> ~8.500.000</span>
+						</li>
+						<li id>
+							<strong><a href=""> Du lich Singapo: 4 ngày giá tốt</a></strong> <span> ~8.500.000</span>
+						</li>
+					</ul>
+			</div>
+				</div>
+				
+      </div>
+	  </div>
+	  
+		 <div class="content1">
+	   			
+				<div class="panel panel-primary">
+                
+				<div class="panel-heading">
+                    <h3 class="panel-title">Du lịch nước ngoài</h3>
+                </div>
+                
+				<div class="panel-body">
+	<div class= "form1">
+				<h4> <a href="">Du lịch Hàn Quốc: 4 ngày giá tốt (T11/2015)</a></h4>
+	</div>
+		<div class="thu">
+		
+			<div class="thu1">
+			<img width="100%" height="60%" src="_anh/anhdl1.jpg"  alt="Ảnh test" />
+		</div>
+		<div class="thu2">
+			<table>
+				<tbody>
+				<tr>
+					<td>
+						<strong> Giá từ </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	 1.400.000d
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Giời gian </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	4 Ngày 4 Đêm
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Khời hành </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	24/10;5,13,20/11
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Vận Chuyển </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	Máy bay Vietnam Airlines
+					</td>
+					</tr>
+						<tr>
+					<td>
+						<strong> Khách Sạn </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					  <img src="_anh/sao.gif" />
+					</td>
+					</tr>
+				</tbody>
+			</table>
+			</div>
+	
+				</div>
+				<div class= "clear"></div>
+		<div class="noidung">
+					<ul>
+					
+						<li >
+							<strong><a href=""> Du lich Singapo: 4 ngày giá tốt</a></strong> <span> ~8.500.000</span>
+						</li>
+						<li id>
+						 	<strong><a href=""> Du lich Singapo: 4 ngày giá tốt</a></strong> <span> ~8.500.000</span>
+						</li>
+						<li id>
+							<strong><a href=""> Du lich Singapo: 4 ngày giá tốt</a></strong> <span> ~8.500.000</span>
+						</li>
+						<li id>
+							<strong><a href=""> Du lich Singapo: 4 ngày giá tốt</a></strong> <span> ~8.500.000</span>
+						</li>
+					</ul>
+			</div>
+				</div>
+				
+         </div>
+		 </div>
+		
+	 <div class="content2">
+		
+		
+		<div class="panel panel-primary">
+                
+				<div class="panel-heading">
+                    <h3 class="panel-title">DICH VU VISA</h3>
+                </div>
+				
+        
+				<div class="panel-body">
+	
+		<div class="visa">
+
+			
+		
+		<div class= "visa1">
+				<h4> <a href="">Lam visa du lich mỹ</a></h4>
+	</div>
+			<div class="visa2">
+			<img width="100%" height="120%" src="_anh/anhvisa.jpg"  alt="Ảnh test" />
+		</div>
+		<div class="visa3">
+			<table>
+				<tbody>
+				<tr>
+					<td>
+						<strong> Giá từ </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	 1.400.000d
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Giời gian lam</strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	14 Ngày
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Loại visa </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	1 năm nhiều lần
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Thời gian thường trú </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	30 ngày
+					</td>
+					</tr>
+						
+				</tbody>
+			</table>
+			</div>
+	
+	</div>
+	
+	<div class="visa">
+
+			
+		
+		<div class= "visa1">
+				<h4> <a href="">Lam visa du lich mỹ</a></h4>
+	</div>
+			<div class="visa2">
+			<img width="100%" height="120%" src="_anh/anhvisa.jpg"  alt="Ảnh test" />
+		</div>
+		<div class="visa3">
+			<table>
+				<tbody>
+				<tr>
+					<td>
+						<strong> Giá từ </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	 1.400.000d
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Giời gian lam</strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	14 Ngày
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Loại visa </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	1 năm nhiều lần
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Thời gian thường trú </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	30 ngày
+					</td>
+					</tr>
+						
+				</tbody>
+			</table>
+			</div>
+	
+	</div>
+	<div class="visa">
+
+			
+		
+		<div class= "visa1">
+				<h4> <a href="">Lam visa du lich mỹ</a></h4>
+	</div>
+			<div class="visa2">
+			<img width="100%" height="120%" src="_anh/anhvisa.jpg"  alt="Ảnh test" />
+		</div>
+		<div class="visa3">
+			<table>
+				<tbody>
+				<tr>
+					<td>
+						<strong> Giá từ </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	 1.400.000d
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Giời gian lam</strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	14 Ngày
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Loại visa </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	1 năm nhiều lần
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Thời gian thường trú </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	30 ngày
+					</td>
+					</tr>
+						
+				</tbody>
+			</table>
+			</div>
+	
+	</div>
+	
+	<div class="visa">
+
+			
+		
+		<div class= "visa1">
+				<h4> <a href="">Lam visa du lich mỹ</a></h4>
+	</div>
+			<div class="visa2">
+			<img width="100%" height="120%" src="_anh/anhvisa.jpg"  alt="Ảnh test" />
+		</div>
+		<div class="visa3">
+			<table>
+				<tbody>
+				<tr>
+					<td>
+						<strong> Giá từ </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	 1.400.000d
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Giời gian lam</strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	14 Ngày
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Loại visa </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	1 năm nhiều lần
+					</td>
+					</tr>
+					<tr>
+					<td>
+						<strong> Thời gian thường trú </strong>
+					</td>
+					<td>
+						 : 
+					</td>
+					<td>
+					 	30 ngày
+					</td>
+					</tr>
+						
+				</tbody>
+			</table>
+			</div>
+	
+	</div>
+	
+				</div>
+				
+		
+				</div>
+				
+         
+		
+		 
+	</div>	
+		
+	   			
+				
+		
+		<div id= "footer">
+		<hr />
+			<div class="container">
+				<div class="row">
+					
+					<div class="col-md-4">
+					
+	   <img height="50%" width="100%" src="_anh/logo2.png"  alt="Ảnh test" />
+	  
+					</div>
+					<div  class="col-md-4 ">
+						<p style="font-size:12px"><b> Công ty cổ phần du lịch Việt</b></p>
+						<p style="font-size:12px"> Giấy phép lữ hành quốc tế :  GP79 - 042/2009/TCDL-GP LHQT</p>
+						<p  style="font-size:12px">Số GCNDT: 0305448565 cấp ngày 15/02/2012 (cấp lần 3)</p>
+						<p  style="font-size:12px"><b>Nguoi đại diên:</b> ông Mai Quốc Tuấn</p>
+						<p  style="font-size:12px"><span class="glyphicon glyphicon-envelope"></span><b> E-Mail:</b>dulichviet@gmail.com</p>
+					</div>
+					
+					<div  class="col-md-4 ">
+								<br />
+											<p  style="font-size:12px"><span class="glyphicon glyphicon-home"></span><b> Văn phòng chính:</b> 175 Nguyễn Thái Bình, Quận 1, Tp. HCM.</p>
+										<p  style="font-size:12px"><span class="glyphicon glyphicon-phone-alt"></span><b> Điện thoại:</b> (+84 8) 7305 6789</p>
+										<p  style="font-size:12px"><span class="glyphicon glyphicon-home"></span><b> Văn phòng chính :</b>121 Lê Thanh Nghị, Hai Bà Trưng, Hà Nội.</p>
+										<p  style="font-size:12px"><span class="glyphicon glyphicon-phone-alt"></span><b> Điện thoại:</b> (+84 4) 3512 3388</p>	 
+
+					</div>
+					
+				</div>
+			</div>
+	    
+		</div>
+	</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Du Lịch Việt Nam</title>
+<link rel="stylesheet" type="text/css" href="css/bodystyles.css" />
+<link rel="stylesheet" type="text/css" href="css/menuleft.css" />
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+
+<link rel="shortcut icon" href="anh/liva_theme_hipc_favicon.ico" type="image/x-icon" />
+</head>
+<body background="anh/1.jpg">
+<table width="950" border="0" align="center" bgcolor="#FFFFFF" >
+  <tr>
+    <td height="142" colspan="3" background="anh/23.jpg" ><?php include "header.php";?></td>
+  </tr>
+  <tr>
+    <td height="40" colspan="3" bgcolor="#0066CC"><?php include "top.php";?></td>
+  </tr>
+  <tr>
+    <td width="150" valign="top"><?php include "left.php";?></td>
+    <td width="800" valign="top"><?php include "center.php";?></td>
+    <td width="200" valign="top"><?php include "right.php"; ?></td>
+  </tr>
+  <tr>
+    <td height="72" colspan="3">
+    <table border="0" cellspacing="0" cellpadding="0">
+        <tr>
+      
+          
+          <td width="350">
+          
+     <img height="90%" width="90%" src="_anh/logo2.png"  alt="Ảnh test" />
+    
+          </td>
+          <td width="400">
+            <p style="font-size:12px"><b> Công ty cổ phần du lịch Việt</b></p>
+            <p style="font-size:12px"> Giấy phép lữ hành quốc tế :  GP79 - 042/2009/TCDL-GP LHQT</p>
+            <p  style="font-size:12px">Số GCNDT: 0305448565 cấp ngày 15/02/2012 (cấp lần 3)</p>
+            <p  style="font-size:12px"><b>Nguoi đại diên:</b> ông Mai Quốc Tuấn</p>
+            <p  style="font-size:12px"><span class="glyphicon glyphicon-envelope"></span><b> E-Mail:</b>dulichviet@gmail.com</p>
+          </td>
+          
+          <td width="400">
+                <br />
+                      <p  style="font-size:12px"><span class="glyphicon glyphicon-home"></span><b> Văn phòng chính:</b> 121 Lê Thanh Nghị, Hai Bà Trưng, Hà Nội.</p>
+                    <p  style="font-size:12px"><span class="glyphicon glyphicon-phone-alt"></span><b> Điện thoại:</b> (+84 8) 7305 6789</p>
+                    <p  style="font-size:12px"><span class="glyphicon glyphicon-home"></span><b> Liên hệ quảng cáo:</b> ông Nguyễn Nhật Minh Tú</p>
+                    <p  style="font-size:12px"><span class="glyphicon glyphicon-phone-alt"></span><b> Điện thoại:</b> (+84) 1693987897</p>  
+
+          </td>
+          
+        
+      </tr>
+    </td>
+  </tr>
+</table>
+</body>
+</html>
